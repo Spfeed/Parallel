def4d519208d651b45b345ed3be6348567f6b01a
Spfeed <subbart2266@gmail.com>
2023-11-29 02:25:45 +0400

    4.4 готова

--- Parallel/Parallel.cpp
+++ Parallel/Parallel.cpp
@@ -1,48 +1,104 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <vector>
 #include <omp.h>
 
 using namespace std;
 
-// Функция, для которой вычисляется интеграл
-double f(double x) {
-    return sin(x);
+// Тип для матрицы
+typedef vector<vector<double>> Matrix;
+
+// Тип для вектора
+typedef vector<double> Vector;
+
+// Функция для создания и заполнения матрицы случайными значениями
+Matrix createRandomMatrix(int rows, int cols) {
+    Matrix mat(rows, Vector(cols));
+#pragma omp parallel for
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            mat[i][j] = static_cast<double>(rand()) / RAND_MAX;
+        }
+    }
+    return mat;
 }
 
-// Функция для вычисления интеграла методом Симпсона с использованием OpenMP
-double integrateSimpson(double a, double b, int n) {
-    double h = (b - a) / n;
-    double sum = f(a) + f(b);
+// Функция для вывода матрицы
+void printMatrix(const Matrix& mat) {
+    for (const auto& row : mat) {
+        for (double value : row) {
+            cout << value << " ";
+        }
+        cout << endl;
+    }
+}
 
-#pragma omp parallel for reduction(+:sum)
-    for (int i = 1; i < n; i++) {
-        double x = a + i * h;
-        sum += (i % 2 == 0) ? 2 * f(x) : 4 * f(x);
+// Функция для вывода вектора
+void printVector(const Vector& vec) {
+    for (double value : vec) {
+        cout << value << " ";
+    }
+    cout << endl;
+}
+
+// Функция для вычисления выражения b * (A - C) * b^T
+Matrix calculateExpression(const Vector& b, const Matrix& A, const Matrix& C) {
+    int rows_A = A.size();
+    int cols_A = A[0].size();
+    int rows_b = b.size();
+
+    Matrix result(rows_b, Vector(rows_b, 0.0));
+
+#pragma omp parallel for
+    for (int i = 0; i < rows_b; ++i) {
+        for (int j = 0; j < rows_b; ++j) {
+            double sum = 0.0;
+            for (int k = 0; k < cols_A; ++k) {
+                sum += b[k] * (A[k][j] - C[k][j]);
+            }
+            result[i][j] = sum;
+        }
     }
 
-    return h / 3 * sum;
+    return result;
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    double a, b;
-    int n;
+    srand(static_cast<unsigned int>(time(0)));
 
-    cout << "Введите нижний предел интегрирования (a): ";
-    cin >> a;
+    // Задаем размерности матриц и вектора
+    int rows_A = 3, cols_A = 3;
+    int rows_b = 3, cols_b = 1;
 
-    cout << "Введите верхний предел интегрирования (b): ";
-    cin >> b;
+    // Создаем и заполняем матрицы A, C и вектор b случайными значениями
+    Matrix A = createRandomMatrix(rows_A, cols_A);
+    Matrix C = createRandomMatrix(rows_A, cols_A);
+    Vector b(rows_b);
+#pragma omp parallel for
+    for (int i = 0; i < rows_b; ++i) {
+        b[i] = static_cast<double>(rand()) / RAND_MAX;
+    }
 
-    cout << "Введите количество разбиений (n): ";
-    cin >> n;
+    // Вывод начальных матриц и векторов
+    cout << "Матрица A:\n";
+    printMatrix(A);
 
-    // Вычисление интеграла
-    double result = integrateSimpson(a, b, n);
+    cout << "\nМатрица C:\n";
+    printMatrix(C);
 
-    // Вывод результата
-    cout << "Значение интеграла: " << result << endl;
+    cout << "\nВектор b:\n";
+    printVector(b);
+
+    // Последовательное вычисление
+    Matrix result_sequential = calculateExpression(b, A, C);
+    cout << "\nРезультат (последовательно):\n";
+    printMatrix(result_sequential);
+
+    // Параллельное вычисление
+    Matrix result_parallel = calculateExpression(b, A, C);
+    cout << "\nРезультат (параллельно):\n";
+    printMatrix(result_parallel);
 
     return 0;
 }
